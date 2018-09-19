1df97481b9659c4198c4d301596612a6c8ffc818
Anand Singh <sanand926@gmail.com>
2018-09-19 19:33:35 +0530

    created: button component

---
+++ src/components/form/button/button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button } from 'antd'
+
+const btn = (props) => {
+    return(
+        <Button 
+            htmlType={props.htmlTypes} 
+            block={props.isBlock ? true : false}
+            size={props.isSize ? props.isSize : 'large'}
+            type={props.isType ? props.isType : 'default'}
+            disabled={props.isDisabled ? props.isDisabled : false}
+            shape={props.shape}
+        >
+            {props.children}
+        </Button>
+    )
+}
+
+export default btn
