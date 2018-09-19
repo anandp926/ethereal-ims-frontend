94e138cbc588b6b00ce7955cb8d9344d66770be8
Anand Singh <sanand926@gmail.com>
2018-09-19 19:35:22 +0530

    created: form-control component

---
+++ src/components/form/form-controls/form-control.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const formControl = (props) => {
+    return(
+        <div className="form-control">
+            {props.children}
+        </div>
+    )
+}
+
+export default formControl
