947aa12c0f6404261eaa0d1f0c70ba2c35244f28
Anand Singh <sanand926@gmail.com>
2018-09-26 11:31:11 +0530

    added: heading component

---
+++ src/components/form/heading/heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const heading = (props) => {
+    return(
+        <div className="heading">
+            <h1>{props.heading}</h1>
+        </div>
+    )
+}
+
+export default heading
