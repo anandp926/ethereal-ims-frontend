f97278a4c6c69edeae1b35e69b8cd20ed89321d4
Anand Singh <sanand926@gmail.com>
2018-09-26 11:36:33 +0530

    created: content-layout component

---
+++ src/components/layout/content-layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Layout } from 'antd'
+
+const { Content, Footer } = Layout
+
+const contentLayout = (props) => {
+    return(
+        <Layout className={props.contentslide ? "content-pull-left" : "content-pull-right"}>
+            <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                {props.children}
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+                Copyright © 2018. Ethereal Machines Pvt Ltd. All rights reserved.
+            </Footer>
+        </Layout>
+    )
+}
+
+export default contentLayout
