f5dbda364c88496e9f229ce744eab4927822bd7a
Anand Singh <sanand926@gmail.com>
2018-09-26 11:43:22 +0530

    created: dashboard component

---
+++ src/distributor/pages/dashboard/dashboard.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+    render(){
+        return(
+            <div className="dashboard">
+                ...
+                <br />
+                Really, HI this is Anand Singh fron India.
+                <br />...<br />...<br />...<br />
+                long
+                <br />...<br />...<br />...<br />...<br />...<br />...
+                <br />...<br />...<br />...<br />...<br />...<br />...
+                <br />...<br />...<br />...<br />...<br />...<br />...
+                <br />...<br />...<br />...<br />...<br />...<br />...
+                <br />...<br />...<br />...<br />...<br />...<br />...
+                <br />...<br />...<br />...<br />...<br />...<br />...
+                <br />...<br />...<br />...<br />...<br />...<br />
+                content
+            </div>
+        )
+    }
+}
+
+export default Dashboard
