07921a9f1239cb34b241f930b97792a501100033
Anand Singh <sanand926@gmail.com>
2018-09-26 11:45:18 +0530

    created: sider nav component

---
+++ src/distributor/sider/sider-nav.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Sider from '../../components/layout/sider'
+import Menu from '../../components/layout/menus/menu'
+import MenuItem from '../../components/layout/menus/menu-item/menu-item'
+import SubMenu from '../../components/layout/menus/submenu/sub-menu'
+
+const siderNav = (props) => {
+    return(
+            <Sider {...props}>
+                <Menu themetype="dark" modetype="inline" selectedkey={["/dashboard"]}>
+                    <MenuItem menutitle="Dashboard" path="/dashboard" icontype="dashboard"/>
+                    <SubMenu submenutitle="Issues" subkey="issue" icontype="exclamation-circle">
+                        <MenuItem menutitle="Create New" path="/issues/new" icontype="plus-circle"/>
+                        <MenuItem menutitle="Pending" path="/issues/pending" icontype="warning"/>
+                        <MenuItem menutitle="Resolved" path="/issues/resolved" icontype="issues-close"/>
+                    </SubMenu>
+                    <MenuItem menutitle="Inventory" path="/inventory" icontype="shopping-cart"/>
+                </Menu>
+            </Sider>
+        )
+}
+
+export default siderNav
