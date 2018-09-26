7d13ca333f76d03caebc94b6ce0c11402429aa8e
Anand Singh <sanand926@gmail.com>
2018-09-26 11:38:15 +0530

    created: menu component

---
+++ src/components/layout/menus/menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Menu } from 'antd'
+
+const menus = (props) => {
+    return(
+        <Menu theme={props.themetype} mode={props.modetype} defaultSelectedKeys={props.selectedkey} {...props}>
+            {props.children}
+        </Menu>
+    )
+}
+
+export default menus
