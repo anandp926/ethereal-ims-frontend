42c015f349e43b16eabd562d9fb735d5b4e7e44b
Anand Singh <sanand926@gmail.com>
2018-09-27 11:22:30 +0530

    added: pending issues route

--- src/distributor/routes/routes.jsx
+++ src/distributor/routes/routes.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Dashboard from '../pages/dashboard/dashboard'
 import AddNew from '../pages/issues/new/isuue-form'
+import PendingIssues from '../pages/issues/pending/pending-issue'
 
 class Routes extends Component {
   componentDidMount() {
@@ -12,6 +13,7 @@ class Routes extends Component {
     return (
       <Switch>
           <Route path="/issues/new" exact component={AddNew} />
+          <Route path="/issues/pending" exact component={PendingIssues} />
           <Route path="/dashboard" exact component={Dashboard} />
           <Route path="/" exact component={Dashboard} />
         </Switch>
