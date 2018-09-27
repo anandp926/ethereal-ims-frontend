7c48ed3c46c68e852939b1e252305bce90808580
Anand Singh <sanand926@gmail.com>
2018-09-27 11:21:08 +0530

    modified: validation in form

--- src/distributor/pages/issues/new/isuue-form.jsx
+++ src/distributor/pages/issues/new/isuue-form.jsx
@@ -14,13 +14,19 @@ class IssueForm extends Component {
         productsNo: '',
         subject: '',
         message:'',
-        step1: false,
-        step2: false
+        productsWarn: false,
+        productsNoWarn: false,
+        subjectWarn: false,
+        messageWarn: false
     }
     
     onFormSubmit =(e)=> {
         e.preventDefault()
-        console.log(this.state.products,this.state.productsNo,this.state.subject,this.state.message)
+        if(this.validate()){
+            console.log(this.state.products,this.state.productsNo,this.state.subject,this.state.message)
+        }else{
+            this.outlineColor()
+        }
     }
 
     onSelectChange = (value) => {
@@ -35,42 +41,79 @@ class IssueForm extends Component {
         })
     }
 
+    outlineColor = () => {
+        if(!this.state.products){
+            this.setState({productsWarn:true})
+        }else{
+            this.setState({productsWarn:false})
+        } 
+        if(!this.state.productsNo){
+            this.setState({productsNoWarn: true})
+        }else{
+            this.setState({productsNoWarn: false})
+        } 
+        if(!this.state.subject){
+            this.setState({subjectWarn: true})
+        }else{
+            this.setState({subjectWarn: false})
+        } 
+        if(!this.state.message){
+            this.setState({messageWarn: true})
+        }else{
+            this.setState({messageWarn: false})
+        }
+    }
+
+    validate = () => {
+        const { products, productsNo, subject, message } = this.state
+        return products !== '' && productsNo !== '' && subject !== '' && message !== ''
+    }    
+
     render(){
         const productsName = ['Ethereal Ray', 'Ethereal Halo', 'Ethereal Pentagram'];
         const productsNumber = ['er12asd ', 'er73dsf', 'er19iyu'];
 
         return(
-            <div className="issue-form-box">
+            <div className="container">
                 <div className="issue-form">
                     <Heading heading={"Raise Issue"}/>
                     <Form onSubmitHandler={this.onFormSubmit}>
                         <AutoComplete  
-                            onSelectChange={(value) => this.setState({products:value, step1:true})}
-                            required 
+                            onSelectChange={(value) => this.setState({products:value})}
+                            isRequired={true}
+                            name="productsName"
+                            labelName="Product Name"
                             dataSource={productsName} 
                             placeholder="Select a Product"
+                            classValue={this.state.productsWarn ? 'inputField-outline' : null}
                         />
                         <AutoComplete  
-                            onSelectChange={(value) => this.setState({productsNo:value, step2: true})}
-                            required 
+                            onSelectChange={(value) => this.setState({productsNo:value})}
+                            isRequired={true}
+                            name="productsNumber"
+                            labelName="Product Number"
                             dataSource={productsNumber} 
                             placeholder="Select Product Number"
+                            classValue={this.state.productsNoWarn ? 'inputField-outline' : null}
                         />
                         <InputType 
                             type="text"
                             value={this.state.subject}
                             name="subject" 
+                            labelName="Subject"
                             placeholder="Enter subject"
                             onInputChange={this.onInputChange}
                             isRequired={true}
-                            isReadonly={this.state.step1 && this.state.step2 ? false : true}
+                            classValue={this.state.subjectWarn ? 'inputField-outline' : null}
                         />
                         <TextArea 
-                            name="message" 
+                            name="message"
+                            labelName="Description" 
                             placeholder="Enter Description" 
                             value={this.state.message} 
                             onInputChange={this.onInputChange} 
                             isRequired={true}
+                            classValue={this.state.messageWarn ? 'inputField-outline' : null}
                         />
                         <div className="issue-form-button" >
                             <Button isType='primary' htmlTypes='submit' isBlock={true}>Submit</Button>
