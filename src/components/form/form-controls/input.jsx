f61ff59d329df4dd609a04bb992aedad49e90f2e
Anand Singh <sanand926@gmail.com>
2018-09-27 11:19:42 +0530

    added: label and class name

--- src/components/form/form-controls/input.jsx
+++ src/components/form/form-controls/input.jsx
@@ -5,8 +5,13 @@ import FormControl from './form-control'
 const inputType =(props)=> {
     return(
         <FormControl>
+            <label htmlFor={props.name}>
+                {props.labelName}
+                {props.isRequired ? <span className="star">*</span> : null}
+            </label>
             <Input
                 type={props.type}
+                className={props.classValue}
                 placeholder={props.placeholder}
                 prefix={props.prefixType ? <Icon type={props.prefixType} style={{ color: 'rgba(0,0,0,.25)' }} /> : null}
                 name={props.name}
