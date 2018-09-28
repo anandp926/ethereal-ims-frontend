f264d147822cb4d10372cbdf37f1d9534494b6ba
Anand Singh <sanand926@gmail.com>
2018-09-28 11:37:33 +0530

    refactore: removed unwanted and added components

--- src/distributor/pages/issues/pending/pending-issue.jsx
+++ src/distributor/pages/issues/pending/pending-issue.jsx
@@ -1,95 +1,82 @@
 import React, { Component } from 'react'
-import { Divider } from 'antd';
 import Button from '../../../../components/form/button/button'
+import Card from '../../../../components/ui/card/card'
+import CardHeading from '../../../../components/ui/card/card-heading/card-heading'
+import Divider from '../../../../components/ui/divider/divider'
+import MsgBox from '../../../../components/ui/user-msg/user-msg-box'
+import { Link } from 'react-router-dom'
 import './pending-issue.css'
 
 class PendingIssue extends Component {
+
     render(){
         return(
             <div className="container flex-column">
                 <div className="pending-issue-list">
-                    <div className="card">
-                        <div className="card-container">
-                            <div className="card-heading">
-                                <h4><b>v9530: Ray Mising Wing not[Pending]</b></h4> 
+                    <Card>
+                        <CardHeading 
+                            title="v9530: Ray Mising Wing not[Pending]"
+                            subtitle={
                                 <div>
                                     <span>27 Sep 2018</span>
-                                    <span >
-                                        <Button isType='primary' htmlTypes='submit' isSize="small" classValue="chip">
-                                            Edit
-                                        </Button>
-                                    </span>
                                     <span>
                                         <Button isType='danger' htmlTypes='submit' isSize="small" classValue="chip">
                                             Delete
                                         </Button>
                                     </span>
                                     <span>
-                                        <Button isType='ghost' htmlTypes='submit' isSize="small" classValue="chip">
-                                            Resolved
+                                        <Button isType='default' htmlTypes='submit' isSize="small" classValue="chip">
+                                            <Link to="pending/1">More</Link>
                                         </Button>
                                     </span>
                                 </div>
-                            </div>
-                            <Divider className="divider"/>
-                            <div className="user-msg-box">
-                                <div className="user-name">
-                                    <h4><b>John Doe</b></h4>
-                                    <p>5:30PM, 27 Sep 2018</p>
-                                </div>
-                                <div className="msg-box">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                    Tellus in metus vulputate eu scelerisque felis imperdiet. Quis vel eros donec ac odio tempor orci dapibus ultrices. 
-                                    Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Scelerisque eu ultrices vitae auctor eu. 
-                                    Tristique senectus et netus et malesuada fames ac. Orci ac auctor augue mauris augue neque gravida in fermentum. 
-                                    In metus vulputate eu scelerisque felis imperdiet proin. Vel pretium lectus quam id leo in vitae. 
-                                    Ac tortor dignissim convallis aenean et tortor at.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                            }
+                        />
+                        <Divider/>
+                        <MsgBox 
+                            title="John Deo"
+                            subtitle="5:30PM, 27 Sep 2018"
+                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Tellus in metus vulputate eu scelerisque felis imperdiet. Quis vel eros donec ac odio tempor orci dapibus ultrices. 
+                            Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Scelerisque eu ultrices vitae auctor eu. 
+                            Tristique senectus et netus et malesuada fames ac. Orci ac auctor augue mauris augue neque gravida in fermentum. 
+                            In metus vulputate eu scelerisque felis imperdiet proin. Vel pretium lectus quam id leo in vitae. 
+                            Ac tortor dignissim convallis aenean et tortor at."
+                        />
+                    </Card>
                 </div>
                 <div className="pending-issue-list">
-                    <div className="card">
-                        <div className="card-container">
-                            <div className="card-heading">
-                                <h4><b>v9530: Ray Mising Wing not[Pending]</b></h4> 
+                <Card>
+                        <CardHeading 
+                            title="v9530: Ray Mising Wing not[Pending]"
+                            subtitle={
                                 <div>
                                     <span>27 Sep 2018</span>
-                                    <span>
-                                        <Button isType='primary' htmlTypes='submit' isSize="small" classValue="chip">
-                                            Edit
-                                        </Button>
-                                    </span>
                                     <span>
                                         <Button isType='danger' htmlTypes='submit' isSize="small" classValue="chip">
                                             Delete
                                         </Button>
                                     </span>
                                     <span>
-                                        <Button isType='ghost' htmlTypes='submit' isSize="small" classValue="chip">
-                                            Resolved
+                                        <Button isType='default' htmlTypes='submit' isSize="small" classValue="chip">
+                                            <Link to="pending/2">More</Link>
                                         </Button>
                                     </span>
                                 </div>
-                            </div>
-                            <Divider className="divider"/>
-                            <div className="user-msg-box">
-                                <div className="user-name">
-                                    <h4><b>John Doe</b></h4>
-                                    <p>5:30PM, 27 Sep 2018</p>
-                                </div>
-                                <div className="msg-box">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                    Tellus in metus vulputate eu scelerisque felis imperdiet. Quis vel eros donec ac odio tempor orci dapibus ultrices. 
-                                    Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Scelerisque eu ultrices vitae auctor eu. 
-                                    Tristique senectus et netus et malesuada fames ac. Orci ac auctor augue mauris augue neque gravida in fermentum. 
-                                    In metus vulputate eu scelerisque felis imperdiet proin. Vel pretium lectus quam id leo in vitae. 
-                                    Ac tortor dignissim convallis aenean et tortor at.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                            }
+                        />
+                        <Divider classValue="divider"/>
+                        <MsgBox 
+                            title="John Deo"
+                            subtitle="5:30PM, 27 Sep 2018"
+                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Tellus in metus vulputate eu scelerisque felis imperdiet. Quis vel eros donec ac odio tempor orci dapibus ultrices. 
+                            Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Scelerisque eu ultrices vitae auctor eu. 
+                            Tristique senectus et netus et malesuada fames ac. Orci ac auctor augue mauris augue neque gravida in fermentum. 
+                            In metus vulputate eu scelerisque felis imperdiet proin. Vel pretium lectus quam id leo in vitae. 
+                            Ac tortor dignissim convallis aenean et tortor at."
+                        />
+                    </Card>
                 </div>
             </div>
         )
