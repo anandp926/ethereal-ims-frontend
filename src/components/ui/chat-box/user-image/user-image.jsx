259f3a522854e329fca164ca83b8de583007b53b
Anand Singh <sanand926@gmail.com>
2018-10-01 12:33:00 +0530

    created: chat user imgae component

---
+++ src/components/ui/chat-box/user-image/user-image.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './user-image.css'
+
+// src={props.isURL}
+
+const arrowBoxRight = (props) => {
+    return(
+        <img 
+            className="circle" 
+            src='https://us.123rf.com/450wm/nexusby/nexusby1805/nexusby180500076/100911331-stock-vector-default-avatar-photo-placeholder-profile-picture.jpg?ver=6'
+            alt="user"
+        />
+    )
+}
+
+export default arrowBoxRight
