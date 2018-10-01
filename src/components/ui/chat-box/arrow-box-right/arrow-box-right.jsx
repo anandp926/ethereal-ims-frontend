d8bf363d6020d08271f2f59fce30429214c6116d
Anand Singh <sanand926@gmail.com>
2018-10-01 12:35:18 +0530

    created: right arrow box component

---
+++ src/components/ui/chat-box/arrow-box-right/arrow-box-right.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './arrow-box-right.css'
+
+const arrowBoxRight = (props) => {
+    return(
+        <div className="arrow_box_right">
+            {props.children}
+        </div>
+    )
+}
+
+export default arrowBoxRight
