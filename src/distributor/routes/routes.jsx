6f7e667d173a0ea23023760260f89bd5fe1a50d5
Anand Singh <sanand926@gmail.com>
2018-10-03 11:14:09 +0530

    added: pending routing

--- src/distributor/routes/routes.jsx
+++ src/distributor/routes/routes.jsx
@@ -4,6 +4,8 @@ import Dashboard from '../pages/dashboard/dashboard'
 import AddNew from '../pages/issues/new/isuue-form'
 import PendingIssues from '../pages/issues/pending/pending-issue'
 import PendingIssueChat from '../pages/issues/pending/pending-issue-chat/pending-issue-chat'
+import ResolvedIssue from '../pages/issues/resolved/resolved-issue'
+import ResolvedIssueChat from '../pages/issues/resolved/resolved-issue-chat/resolved-issue-chat'
 
 class Routes extends Component {
   componentDidMount() {
@@ -16,6 +18,8 @@ class Routes extends Component {
           <Route path="/issues/new" exact component={AddNew} />
           <Route path="/issues/pending" exact component={PendingIssues} />
           <Route path="/issues/pending/:id" exact component={PendingIssueChat} />
+          <Route path="/issues/resolved" exact component={ResolvedIssue} />
+          <Route path="/issues/resolved/:id" exact component={ResolvedIssueChat} />
           <Route path="/dashboard" exact component={Dashboard} />
           <Route path="/" exact component={Dashboard} />
         </Switch>
