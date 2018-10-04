e4899395bdbd91223d74c5ae5eefe5d3ac6d018e
Anand Singh <sanand926@gmail.com>
2018-10-04 12:01:04 +0530

    created: inventory component

---
+++ src/distributor/pages/inventory/inventory.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Inventory extends Component {
+    render(){
+        return(
+            <div>HI this is Anand Singh. and i am in the inventory component</div>
+        )
+    }
+}
+
+export default Inventory
