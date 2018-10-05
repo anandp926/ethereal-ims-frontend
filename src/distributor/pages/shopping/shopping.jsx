eee3acce4cd07779982bbf261f9c8acfe4ad164a
Anand Singh <sanand926@gmail.com>
2018-10-05 19:34:02 +0530

    modified: added link and removed modal

--- src/distributor/pages/shopping/shopping.jsx
+++ src/distributor/pages/shopping/shopping.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Card from '../../../components/ui/card/card'
 import './shopping.css'
 import HaloImage from './assets/halo/halo.png'
@@ -6,80 +7,68 @@ import RayImage from './assets/ray/ray.png'
 import PentagramImage from './assets/pentagram/pentagram.png'
 import Button from '../../../components/form/button/button'
 import InputType from '../../../components/form/form-controls/input'
+import Modal from '../../../components/layout/modal/modal'
 
 
 class Shopping extends Component {
 
-    state = {
-        qty:''
-    }
-
-    onInputChange = (e) => {
-        this.setState({
-            [e.target.name]:e.target.value
-        })
-    }
-    
     render(){
         return(
-            <div>
-                <Card>
-                    <div className="shopping-card-content">
-                        <div className="shopping-card-img">
-                            <img src={HaloImage} alt="halo" className="shopping-card-image"/>
+            <div className="shopping-list">
+                <div className="shopping-item">
+                    <Card>
+                        <div className="shopping-card-content">
+                            <div className="shopping-card-img">
+                                <img src={HaloImage} alt="halo" className="shopping-card-image"/>
+                            </div>
+                            <div className="shopping-card-detail">
+                                <h4>Halo</h4>
+                                <h4 className="price">&#8377; 14,99,999</h4>
+                            </div>
+                            <div className="shopping-card-buy">
+                                <Link to="/shopping/details/1">
+                                    <Button isType="danger">BUY</Button>
+                                </Link>
+                            </div>
                         </div>
-                        <div className="shopping-card-detail">
-                            <h2>Ethereal Halo</h2>
-                            <h5>The Dawn of Hybrid Manufacturing</h5>
-                            <h2 className="price">&#8377; 14,99,999</h2>
-                            <p className="know-more ">Know more?</p>
+                    </Card>
+                </div>
+                <div className="shopping-item">
+                    <Card>
+                        <div className="shopping-card-content">
+                            <div className="shopping-card-img">
+                                <img src={RayImage} alt="ray" className="shopping-card-image"/>
+                            </div>
+                            <div className="shopping-card-detail">
+                                <h4>Ray</h4>
+                                <h4 className="price">&#8377; 14,99,999</h4>
+                            </div>
+                            <div className="shopping-card-buy">
+                                <Link to="/shopping/details/2">
+                                    <Button isType="danger">BUY</Button>
+                                </Link>
+                            </div>
                         </div>
-                        <div className="shopping-card-buy">
-                            <Button isType="danger" >Buy Now</Button>
+                    </Card>
+                </div>
+                <div className="shopping-item">
+                    <Card>
+                        <div className="shopping-card-content">
+                            <div className="shopping-card-img">
+                                <img src={PentagramImage} alt="pentagram" className="shopping-card-image"/>
+                            </div>
+                            <div className="shopping-card-detail">
+                                <h4>Pentagram</h4>
+                                <h4 className="price">&#8377; 14,99,999</h4>
+                            </div>
+                            <div className="shopping-card-buy">
+                                <Link to="/shopping/details/3">
+                                    <Button isType="danger">BUY</Button>
+                                </Link>
+                            </div>
                         </div>
-                    </div>
-                </Card>
-                <Card>
-                    <div className="shopping-card-content">
-                        <div className="shopping-card-img">
-                            <img src={RayImage} alt="ray" className="shopping-card-image"/>
-                        </div>
-                        <div className="shopping-card-detail">
-                            <h2>Ethereal ray</h2>
-                            <h5>A smart dual extruder printer for your everyday 3D printing needs!</h5>
-                            <h2 className="price">&#8377; 14,99,999</h2>
-                            <p className="know-more ">Know more?</p>
-                        </div>
-                        <div className="shopping-card-buy">
-                            <Button isType="danger" >Buy Now</Button>
-                        </div>
-                    </div>
-                </Card>
-                <Card>
-                    <div className="shopping-card-content">
-                        <div className="shopping-card-img">
-                            <img src={PentagramImage} alt="pentagram" className="shopping-card-image"/>
-                        </div>
-                        <div className="shopping-card-detail">
-                            <h2>Ethereal Pentagram</h2>
-                            <h5>Desktop Simultaneous 5 Axis CNC Machine</h5>
-                            <h2 className="price">&#8377; 14,99,999</h2>
-                            <p className="know-more ">Know more?</p>
-                        </div>
-                        <div className="shopping-card-buy">
-                            <Button isType="danger" >Buy Now</Button>
-                            <InputType 
-                                type="number"
-                                value={this.state.qty}
-                                name="qty" 
-                                labelName="qty"
-                                placeholder="qty"
-                                onInputChange={this.onInputChange}
-                                isRequired={true}
-                            />
-                        </div>
-                    </div>
-                </Card>
+                    </Card>
+                </div>
             </div>
         )
     }
