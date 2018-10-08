92b69c58209f50596f38119ee6eb1ac6134fab88
Anand Singh <sanand926@gmail.com>
2018-10-08 18:38:00 +0530

    created: admin component

---
+++ src/admin/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Admin extends Component {
+    render(){
+        return(
+            <div>
+                Hi this is Anand Singh.
+                I am the admin of this website.
+            </div>
+        )
+    }
+}
+
+export default Admin
