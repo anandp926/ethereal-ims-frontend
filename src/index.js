05451d166f633856d4a96665de1a8dd4e6370d1b
Anand Singh <sanand926@gmail.com>
2018-11-14 11:20:18 +0530

    Added: reducers and hashrouting

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import {applyMiddleware, compose, createStore} from 'redux';
+import {Provider} from 'react-redux'
+import rootReducer from './store/reducer'
 import registerServiceWorker from './registerServiceWorker';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
 ReactDOM.render(
-    <BrowserRouter basename='/'>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <HashRouter basename='/'>
+            <App />
+        </HashRouter>
+    </Provider>,
     document.getElementById('root')
 );
 registerServiceWorker();
