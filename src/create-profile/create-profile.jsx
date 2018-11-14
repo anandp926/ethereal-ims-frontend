e97882c07988fa05594c24a20d50212a66661d4e
Anand Singh <sanand926@gmail.com>
2018-11-14 11:24:28 +0530

    Created: user profile creater component

---
+++ src/create-profile/create-profile.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class CreateProfile extends Component {
+    render(){
+        return(
+            <div>Fill details for your profile</div>
+        )
+    }
+}
+
+export default CreateProfile
