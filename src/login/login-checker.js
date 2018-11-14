4db7a36cfd792948aa809df9f408f32447477ae8
Anand Singh <sanand926@gmail.com>
2018-11-14 11:28:38 +0530

    Created: authentication checker before entering app

---
+++ src/login/login-checker.js
@@ -0,0 +1,98 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import { Layout } from 'antd'
+import {withRouter, Redirect} from 'react-router-dom';
+import * as actionType from '../store/actions/action-type'
+import Header from '../components/header/header'
+import Layouts from '../components/layout/layout'
+import ContentLayout from '../components/layout/content-layout'
+import Sider from '../distributor/sider/sider-nav'
+import refreshToken from '../services/apis/refresh-token'
+import {GetToken, SetToken} from '../helpers/token'
+
+class LoginChecker extends Component {
+      state = {
+        contentSlide: false
+    }
+
+    callback = (data) => {
+      if(data.status === 200){
+        SetToken(data.data.nToken, 5);
+        this.props.basic(data.data.user);
+      }else if(data.response){
+        console.log(data.response)
+      }
+    }
+
+    componentDidMount() {
+
+        const {currentURL, isLoggedIn, history, setRedirectUrl } = this.props;
+        if (!isLoggedIn) {
+          // set the current url/path for future redirection (we use a Redux action)
+          // then redirect (we use a React Router method)
+          setRedirectUrl(currentURL);
+          history.push('/');
+        }else if(isLoggedIn){
+          setRedirectUrl(currentURL);
+          refreshToken(this.callback, {token:GetToken()});
+        }
+    }
+
+    contentSlider = (data) => {
+      this.setState({
+          contentSlide:data
+      })
+    }
+    
+    render() {
+      const { isLoggedIn, isFirst, isApproved} = this.props;
+      if (isLoggedIn && isFirst, isApproved) {
+        return (
+          <Layout>
+            <Header/>
+            <Layouts>
+              <Sider content_slider={this.contentSlider}/>
+              <ContentLayout contentslide={this.state.contentSlide}>
+                {this.props.children}
+              </ContentLayout>
+            </Layouts>
+          </Layout>
+        )
+      } else {
+        return null
+      }
+    }
+}
+    
+    // Grab a reference to the current URL. If this is a web app and you are
+    // using React Router, you can use `ownProps` to find the URL. Other
+    // platforms (Native) or routing libraries have similar ways to find
+    // the current position in the app.
+    function mapStateToProps(state, ownProps) {
+      return {
+        isLoggedIn: state.Login.isLogedIn,
+        userType: state.Basic.basic.group,
+        isFirst: state.Basic.basic.firstTime,
+        isApproved: state.Basic.basic.approved,
+        currentURL: ownProps.location.pathname
+      }
+    }
+
+    function mapDispatchToProps(dispatch) {
+      return{
+        setRedirectUrl: (data) => {
+          dispatch({
+            type: actionType.LOGIN,
+            value: data
+          })
+        },
+        basic: (data) => {
+          dispatch({
+            type: actionType.USER_BASIC,
+            value: data
+          })
+        }
+      }
+    }
+    
+    export default withRouter(connect(mapStateToProps, mapDispatchToProps)(LoginChecker))
