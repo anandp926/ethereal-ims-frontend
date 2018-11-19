4fb954bc9eda69f4314133e7248fa3086c07a098
Anand Singh <sanand926@gmail.com>
2018-11-19 12:21:39 +0530

    Created: select component

---
+++ src/components/form/form-controls/select.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Select } from 'antd';
+import FormControl from './form-control'
+
+const select =(props)=> {
+    return(
+        <FormControl>
+            <label htmlFor={props.name}>
+                {props.labelName}
+                {props.isRequired ? <span className="star">*</span> : null}
+                &nbsp;
+            </label>
+            <Select
+                showSearch
+                className={props.classValue}
+                placeholder={props.placeholder}
+                optionFilterProp="children"
+                onChange={props.onSelect}
+                size="large"
+                required={props.isRequired ? props.isRequired : false}
+                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            >
+                {props.children}
+            </Select>
+        </FormControl>
+        
+    )
+}
+
+export default select
