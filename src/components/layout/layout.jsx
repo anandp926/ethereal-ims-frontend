8afd6f9833020f6fcd515671b2d96fb088813663
Anand Singh <sanand926@gmail.com>
2018-11-19 11:31:11 +0530

    Modified: for styling component'

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -3,7 +3,7 @@ import { Layout } from 'antd'
 
 const layout = (props) => {
     return(
-        <Layout style={{marginTop: 64,}} {...props}>
+        <Layout style={{marginTop: 64}} {...props}>
             {props.children}
         </Layout>
     )
