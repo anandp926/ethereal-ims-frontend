d5f20084ce36f18b550bf8d216e4faa979691ed8
Anand Singh <sanand926@gmail.com>
2018-11-24 15:28:33 +0530

    Created: sider nav role based

---
+++ src/side-nav/sider-nav.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import { Layout, Menu, Icon } from 'antd';
+import { withRouter, Link } from 'react-router-dom'
+const { SubMenu } = Menu;
+const { Sider } = Layout;
+
+const SiderNav = (props) => {
+    if( props.roleType === 'user'){
+        return(
+            <Sider 
+                breakpoint="md"
+                collapsedWidth="0"
+                onCollapse={(collapsed) => {props.content_slider(collapsed)}}
+                className="sider"
+            >
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={[props.location.pathname]}>
+                    <Menu.Item key={"/dashboard"}>
+                        <Link to="/dashboard">
+                            <Icon type="dashboard" />
+                            <span className="nav-text">Dashboard</span>
+                        </Link>
+                    </Menu.Item>
+                    <SubMenu
+                    key='sub1'
+                    title={<span><Icon type="exclamation-circle" /><span>Issues</span></span>}
+                    >
+                        <Menu.Item key="/issues/new">
+                            <Link to="/issues/new">
+                            <Icon type="plus-circle" />
+                            <span className="nav-text">Create New</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="/issues/pending">
+                            <Link to="/issues/pending">
+                            <Icon type="warning" />
+                            <span className="nav-text">Pending</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="/issues/resolved">
+                            <Link to="/issues/resolved">
+                            <Icon type="issues-close" />
+                            <span className="nav-text">Resolved</span>
+                            </Link>
+                        </Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key="/shopping">
+                        <Link to="/shopping">
+                        <Icon type="shopping-cart" />
+                        <span className="nav-text">Shopping</span>
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="/faqs">
+                        <Link to="/faqs">
+                        <Icon type="read" />
+                        <span className="nav-text">FAQ</span>
+                        </Link>
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+        )
+    }else if(props.roleType === 'admin'){
+        return(
+            <Sider 
+                breakpoint="md"
+                collapsedWidth="0"
+                onCollapse={(collapsed) => {props.content_slider(collapsed)}}
+                className="sider"
+            >
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={[props.location.pathname]}>
+                    <Menu.Item key={"/dashboard"}>
+                        <Link to="/dashboard">
+                            <Icon type="dashboard" />
+                            <span className="nav-text">Dashboard</span>
+                        </Link>
+                    </Menu.Item>
+                    <SubMenu
+                    key={'/issues'}
+                    title={<span><Icon type="user" /><span>Users</span></span>}
+                    >
+                        <Menu.Item key="/users/new">
+                            <Link to="/users/new">
+                            <Icon type="user-add" />
+                            <span className="nav-text">Create New</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="/users/pending">
+                            <Link to="/users/pending">
+                            <Icon type="warning" />
+                            <span className="nav-text">Pending User</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="/users/approved">
+                            <Link to="/users/approved">
+                            <Icon type="check" />
+                            <span className="nav-text">Approved User</span>
+                            </Link>
+                        </Menu.Item>
+                    </SubMenu>
+                </Menu>
+            </Sider>
+        )
+    }
+}
+
+export default withRouter(SiderNav)
