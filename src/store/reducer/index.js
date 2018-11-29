9edf1613f64fdd0a94355ad413bf8661ad85eb72
Anand Singh <sanand926@gmail.com>
2018-11-29 12:20:17 +0530

    Added: company reducer

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import login from './login'
 import basic from './basic'
+import company from './company'
 
 const rootReducer = combineReducers({
     Login: login,
-    Basic: basic
+    Basic: basic,
+    Company: company
 });
 
 export default rootReducer
