e2dc6054318c9c34f817029df7831019f96b7611
Anand Singh <sanand926@gmail.com>
2018-11-29 12:38:27 +0530

    Added: company props

--- src/admin/pages/users/create-user/create-user.jsx
+++ src/admin/pages/users/create-user/create-user.jsx
@@ -1,41 +1,94 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux'
 import Modal from '../../../../components/layout/modal/modal';
 import Heading from '../../../../components/form/heading/heading';
 import CreateComapny from './create-company/create-company';
 import CreateUserForm from './create-user-form/create-user-form'
+import {GetToken} from '../../../../helpers/token'
+import Loader from '../../../../components/ui/loader/loader'
+//api
+import getCompanylist from '../../../../services/apis/get-company'
+import * as actionType from '../../../../store/actions/action-type'
 
 class CreateUser extends Component {
 
     state = {
-        visible: false
+        visible: false,
+        showLoader: true
+    }
+
+    callback = (data) => {
+        if(data.status === 200){
+            this.props.getCompany(data.data);
+            this.setState({showLoader: false})
+        }else {
+            console.log(data.response);
+        }
+    }
+
+    componentDidMount(){
+        const token = GetToken();
+        if(token){
+            getCompanylist(this.callback, token);
+        } 
     }
 
     openModal = (bool) => {
         this.setState({visible: bool})
     }
     
-    cancelClick = () => {
-        this.setState({visible: false})
+    // close modal
+    cancelClick = (bool) => {
+        if(bool === false){
+            this.setState({visible: false});
+        }else{
+            this.setState({visible: false})
+        }
     }
 
     render(){
-        return(
-            <div className="container">
-                <div className="issue-form">
-                    <Modal 
-                        heading="Create Company"
-                        showModal={this.state.visible}
-                        cancelClick={this.cancelClick}
-                        footer={null}
-                    >
-                        <CreateComapny okClick={this.okClick}/>
-                    </Modal>
-                    <Heading heading={"Create New User"}/>
-                    <CreateUserForm openModal={this.openModal}/>
+
+        if(this.state.showLoader && !this.props.companies){
+            return <Loader/>
+        }else if(this.props.companies){
+            return(
+                <div className="container">
+                    <div className="issue-form">
+                        <Modal 
+                            heading="Create Company"
+                            showModal={this.state.visible}
+                            cancelClick={this.cancelClick}
+                            footer={null}
+                        >
+                            <CreateComapny cancelClick={this.cancelClick} />
+                        </Modal>
+                        <Heading heading={"Create New User"}/>
+                        <CreateUserForm openModal={this.openModal} companies={this.props.companies}/>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }else {
+            return null
+        }
+        
     }
 }
 
-export default CreateUser
+function mapStateToProps(state) {
+    return{
+        companies: state.Company.company
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return{
+        getCompany: (data) => {
+            dispatch({
+                type: actionType.COMPANY,
+                value: data
+            })
+        }
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateUser)
