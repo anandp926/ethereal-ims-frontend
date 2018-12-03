e1a450ee424db88856446ecb1caa1fdf7cfa09e6
Anand Singh <sanand926@gmail.com>
2018-12-03 15:13:05 +0530

    Created: Pending users list component

---
+++ src/admin/pages/users/user-list/pending/pending.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Table, Divider } from 'antd';
+
+const newUsers = (props) => {
+
+    const columns = [{
+        title: 'Id',
+        dataIndex: '_id',
+        key: '_id',
+        render: text => <a href="javascript:;">{text}</a>,
+    }, {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    }, {
+        title: 'Group',
+        dataIndex: 'group',
+        key: 'group',
+    }, {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+            <span>
+                <a href="javascript:;">Approve</a>
+                <Divider type="vertical" />
+                <a onClick={() => props.openDrawer(true, record._id)}>View</a>
+            </span>
+        ),
+    }];
+
+    const user = props.users.filter((user) => user.approval === 'pending')
+    if(user.length === 0){
+        return null
+    }else {
+        return (
+            <Table 
+                columns={columns} 
+                dataSource={user} 
+                rowKey={record => record._id} 
+                title={() => <h3><b>Pending Users</b></h3>}
+                bordered
+                pagination={false}
+                className={props.classValue}
+            />
+        )
+    }
+}
+
+
+export default newUsers
