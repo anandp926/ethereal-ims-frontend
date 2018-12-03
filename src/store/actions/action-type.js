2b7d47c07fa9f9f3bbdd8aaa65aa2c87ff93fa0a
Anand Singh <sanand926@gmail.com>
2018-12-03 15:09:08 +0530

    Added: users action type

--- src/store/actions/action-type.js
+++ src/store/actions/action-type.js
@@ -4,3 +4,5 @@ export const USER_BASIC = 'BASIC';
 export const FIRST_TIME = 'FIRST_TIME';
 export const COMPANY = 'COMPANY';
 export const ADD_COMPANY = 'ADD_COMPANY'
+export const USERS = 'USERS';
+export const ADD_USER = 'ADD_USER'
