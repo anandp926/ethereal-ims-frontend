5c1b31142765c48c5ab9b47853b6b7996cb3ead5
Anand Singh <sanand926@gmail.com>
2018-12-10 12:44:59 +0530

    Added: Videos data

---
+++ src/distributor/pages/resources/videos/data.js
@@ -0,0 +1,87 @@
+export const videoLink = [
+    {
+        "id": 'a1',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a2',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a3',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a4',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a5',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a6',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a7',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a8',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a9',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a10',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a11',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a12',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a13',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }, {
+        "id": 'a14',
+        'link': 'https://www.youtube.com/embed/zt1k0G9E-Ig?ecver=2',
+        'title': 'Ethereal Machines "Halo" 5-Axis 3D Printer at CES 2018',
+        'description': `Ethereal Machines' new "5-axis" 3D printer is turning heads at CES 2018, and has even won an Innovation Award at the show this year, and for good reason. With adding a rotational axis and translational 
+        axis, the Halo is capable of printing much more complex objects.`
+    }
+];
