bb93b8f1943cf38aff20ee4555180d5399eeffa2
Anand Singh <sanand926@gmail.com>
2018-12-11 17:44:49 +0530

    Created: Certificate Component

---
+++ src/distributor/pages/resources/certificate/certificate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const certificates = () => {
+    return(
+        <div>Hi! I'm Certificate and I'll put weight on your personality.</div>
+    )
+}
+
+export default certificates
