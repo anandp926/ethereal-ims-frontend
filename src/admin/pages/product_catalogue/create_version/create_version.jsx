b8efcc52fc37efab499a9fdcffc6fd28ca95fdc5
Anand Singh <sanand926@gmail.com>
2018-12-14 13:43:26 +0530

    Created: Product Catalog component

---
+++ src/admin/pages/product_catalogue/create_version/create_version.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class CreateVersion extends Component {
+    render(){
+        return(
+            <div>hi</div>
+        )
+    }
+}
+
+export default CreateVersion
