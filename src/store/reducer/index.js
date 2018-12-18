8b273ba1c765615f86ee8be4b91addded7c926c0
Anand Singh <sanand926@gmail.com>
2018-12-18 12:14:16 +0530

    Added: products reducers

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,14 +1,16 @@
-import { combineReducers } from 'redux'
-import login from './login'
-import basic from './basic'
-import company from './company'
-import users from './users'
+import { combineReducers } from 'redux';
+import login from './login';
+import basic from './basic';
+import company from './company';
+import users from './users';
+import products from './product_catalog'
 
 const rootReducer = combineReducers({
     Login: login,
     Basic: basic,
     Company: company,
-    Users: users
+    Users: users,
+    Products: products
 });
 
 export default rootReducer
