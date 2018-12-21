1fc0b36a17487383a1e4ec075ff553754cc466f1
Anand Singh <sanand926@gmail.com>
2018-12-21 19:10:21 +0530

    Modified: added request, redux, etc

--- src/admin/pages/product_catalogue/product_entry/product_entry.jsx
+++ src/admin/pages/product_catalogue/product_entry/product_entry.jsx
@@ -1,41 +1,74 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
 import Form from '../../../../components/form/form';
 import InputType from '../../../../components/form/form-controls/input';
 import Button from '../../../../components/form/button/button';
 import ErrorBox from '../../../../components/form/error-box/error-box';
 import Loader from '../../../../components/ui/loader/loader';
 import Dropdown from '../../../../components/form/form-controls/select';
+import RichTextEditor from 'react-rte'
+import ReactTextEditor from '../../../../components/form/rich-text-editor/rich-text-editor'
 import { Modal } from 'antd'
 import { Table, Select } from 'antd';
+//
+import {GetToken} from '../../../../helpers/token'
+import * as actionType from '../../../../store/actions/action-type'
+import {productEntry} from '../../../../services/apis/product_catalog'
 
 const Option = Select.Option;
 
 class ProductEntry extends Component {
 
     state = {
+        gToken: GetToken(),
         version: '',
         productId: '',
         serialNumber: '',
         boardNumber: '',
+        variant: RichTextEditor.createEmptyValue(),
+        serialNumberWarn: false,
+        boardNumberWarn: false,
+        versionWarn: false,
+        productIdWarn: false,
         errorMsg: false,
         showLoader: false
     };
 
+    callback = (data) => {
+        if(data.status === 200){
+            this.props.machineDispatch(data.data);
+            this.success();
+            this.reset();
+        }else{
+            this.setState({errorMsg: 'Something went wrong. Please try again ', showLoader: false});
+        }
+    }
+
     onFormSubmit = (e) => {
-        const {productId, version} = this.state;
+        this.setState({showLoader: true, errorMsg: ''})
+        const { version, productId, serialNumber, boardNumber, gToken, variant} = this.state;
         e.preventDefault();
 
         const data = {
-            id: productId,
-            version: version
+            version: version,
+            serialNumber: serialNumber,
+            boardNumber: boardNumber,
+            variant: variant.toString('html')
         }
-        console.log(data)
+
+        if(this.validate() && gToken){
+            this.outlineColor();
+            productEntry(this.callback, productId, data, gToken)
+        }else {
+            this.outlineColor();
+            this.setState({errorMsg: 'Please fill all the required field', showLoader: false});
+        }
     };
 
     success = () => {
         Modal.success({
-            title: 'Congratulation',
-            content: 'Your order has been placed. :)',
+            title: 'Successful',
+            content: 'Product version has been created.',
         });
     };
 
@@ -48,64 +81,68 @@ class ProductEntry extends Component {
     };
 
     onVersionSelect = (value) => {
-        this.setState({ versoin: value })
+        this.setState({ version: value })
     };
 
+    textChange = (value) => {
+        this.setState({variant: value});
+    };
+
+    outlineColor = () => {
+        if(!this.state.productId.trim()){
+            this.setState({productIdWarn:true})
+        }else{
+            this.setState({productIdWarn:false})
+        }if(!this.state.version.trim()){
+            this.setState({versionWarn:true})
+        }else{
+            this.setState({versionWarn:false})
+        }if(!this.state.serialNumber.trim()) {
+            this.setState({serialNumberWarn: true})
+        }else{
+            this.setState({serialNumberWarn: false})
+        }if(!this.state.boardNumber.trim()) {
+            this.setState({boardNumberWarn: true})
+        }else{
+            this.setState({boardNumberWarn: false})
+        }
+    }
+
+    validate = () => {
+        const { version, productId, serialNumber, boardNumber} = this.state;
+        return version.trim() !== '' && productId.trim() !== '' && serialNumber.trim() !== '' && boardNumber.trim() !== '';
+    }
+
+    reset = () => {
+        this.setState({
+            serialNumber: '',
+            boardNumber: '',
+            showLoader: false,
+            errorMsg: '',
+            variant: RichTextEditor.createEmptyValue(),
+        })
+    }
+
     render(){
-        const dataSource = [{
-            key: '1',
-            version: 'A12018'
-          }, {
-            key: '2',
-            version: 'A12019'
-          }, {
-            key: '3',
-            version: 'A22019'
-          }, {
-            key: '4',
-            version: 'A32019'
-          }];
-          
           const columns = [{
-            title: 'Version',
+            title: 'Versions',
             dataIndex: 'version',
             key: 'p_version',
           }, {
-            title: 'Details',
-            key: 'details',
-            render: (text, record) => (
-                <span>
-                    <a href="javascript:;">View</a>
-                </span>
-            ),
+            title: 'Machines',
+            dataIndex: 'count',
+            key: 'p_count',
         }, {
             title: 'Action',
             key: 'action',
             render: (text, record) => (
                 <span>
-                    <a href="javascript:;">Edit</a>
+                    <a href="javascript:;">View</a>
                 </span>
             ),
         }];
-          
-        const product = [
-            {
-                '_id': 'dfgds577hsgh',
-                'name': 'Halo',
-                'version': 'asf324'
-            },
-            {
-                '_id': 'dfgdsd77hsgh',
-                'name': 'Ray',
-                'version': 'asf324'
-            },
-            {
-                '_id': 'dfgdufr7hsgh',
-                'name': 'Pentagram',
-                'version': 'asf324'
-            }
-        ];
 
+        let filterProduct = this.props.products.filter((product) => product._id === this.state.productId);
         return(
             <div className="container product">
                 <div className="container-left">
@@ -119,10 +156,10 @@ class ProductEntry extends Component {
                                 size="large"
                                 isRequired={true}
                                 filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-                                classValue={this.state.companyIdWarn ? 'inputField-outline' : null}
+                                classValue={this.state.productIdWarn ? 'inputField-outline' : null}
                             >
                                 {
-                                    product.map((product) => (
+                                    this.props.products.map((product) => (
                                         <Option value={product._id} key={product._id}>{product.name}</Option>
                                     ))
                                 }
@@ -135,12 +172,14 @@ class ProductEntry extends Component {
                                 size="large"
                                 isRequired={true}
                                 filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-                                classValue={this.state.companyIdWarn ? 'inputField-outline' : null}
+                                classValue={this.state.versionWarn ? 'inputField-outline' : null}
                             >
                                 {
-                                    product.map((product) => (
-                                        <Option value={product.vaersion} key={product._id}>{product.version}</Option>
-                                    ))
+                                    filterProduct[0] !== undefined && filterProduct[0] !== null && (
+                                        filterProduct[0].details.map((version) =>(
+                                            <Option value={version.version} key={version._id}>{version.version}</Option>
+                                        ))
+                                    )
                                 }
                             </Dropdown>
                             <ErrorBox errorMsgs={<strong>If products and versions are not listed above then&nbsp;<b>Create Product/Version.</b></strong>}></ErrorBox>
@@ -152,6 +191,7 @@ class ProductEntry extends Component {
                                 placeholder="Enter Product Serial Number"
                                 onInputChange={this.onInputChange}
                                 isRequired={true}
+                                classValue={this.state.serialNumberWarn ? 'inputField-outline' : null}
                             />
                             <InputType 
                                 type="text"
@@ -161,6 +201,14 @@ class ProductEntry extends Component {
                                 placeholder="Enter Product Board Number"
                                 onInputChange={this.onInputChange}
                                 isRequired={true}
+                                classValue={this.state.boardNumberWarn ? 'inputField-outline' : null}
+                            />
+                            <ErrorBox errorMsgs={<strong>If any variant in this machine then&nbsp;<b>Please write down.</b></strong>}></ErrorBox>
+                            <ReactTextEditor
+                                labelName="Variant"
+                                value={this.state.variant}
+                                textChange={this.textChange}
+                                placeholder="Enter Variant of Machine"
                             />
                             { this.state.errorMsg ? <ErrorBox errorMsgs={this.state.errorMsg} /> : null}
                             { this.state.showLoader ? <Loader>Creating...</Loader> : null }
@@ -171,31 +219,35 @@ class ProductEntry extends Component {
                     </div>
                 </div>
                 <div className="container-right">
-                    <Table 
-                        dataSource={dataSource} 
-                        columns={columns}
-                        title={() => <h3><b>Halo</b></h3>}
-                        bordered
-                        pagination={false}
-                     />
-                     <Table 
-                        dataSource={dataSource} 
-                        columns={columns}
-                        title={() => <h3><b>Halo</b></h3>}
-                        bordered
-                        pagination={false}
-                     />
-                     <Table 
-                        dataSource={dataSource} 
-                        columns={columns}
-                        title={() => <h3><b>Halo</b></h3>}
-                        bordered
-                        pagination={false}
-                     />
+                {
+                    this.props.products.map((product) => (
+                        <div key={product._id} className="versionTable">
+                            <Table 
+                                dataSource={product.details} 
+                                columns={columns}
+                                rowKey={record => record._id}
+                                title={() => <h3><b>{product.name}</b>&nbsp;<b>({product.count})</b></h3>}
+                                bordered
+                                pagination={false}
+                            />
+                        </div>
+                    ))
+                }
                 </div>
             </div>
         )
     }
 }
 
-export default ProductEntry
+function mapDispatchToProps (dispatch) {
+    return{
+        machineDispatch: (data) => {
+            dispatch({
+                type: actionType.ADD_MACHINE,
+                value: data
+            })
+        }
+    }
+}
+
+export default connect(null, mapDispatchToProps)(ProductEntry)
