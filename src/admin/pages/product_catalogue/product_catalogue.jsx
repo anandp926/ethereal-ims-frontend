6393eaaaacf5e156d60f9f75c8b9505c470d0d15
Anand Singh <sanand926@gmail.com>
2018-12-21 19:14:27 +0530

    Styled and Modifed

--- src/admin/pages/product_catalogue/product_catalogue.jsx
+++ src/admin/pages/product_catalogue/product_catalogue.jsx
@@ -1,32 +1,78 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 import { Tabs, Icon } from 'antd';
 import CreateProduct from './create_product/create_product';
 import Dashboard from './dashboard/dashboard';
 import CreateVersion from './create_version/create_version';
-import ProductEntry from './product_entry/product_entry';
+import MachineEntry from './product_entry/product_entry';
 import './product_catalogue.css'
+import Loader from '../../../components/ui/loader/loader'
+//
+import * as actionType from '../../../store/actions/action-type';
+import {getAllProduct} from '../../../services/apis/product_catalog';
+import {GetToken} from '../../../helpers/token'
 
 const TabPane = Tabs.TabPane;
 
 class ProductCatalogue extends Component {
+
+    state= {
+        gToken: GetToken(),
+        showLoader: true
+    }
+
+    callback = (data) => {
+        if(data.status === 200){
+            this.props.getProducts(data.data)
+            this.setState({showLoader: false})
+        }else {
+            console.log(data.response)
+        }
+    }
+
+    componentDidMount() {
+        getAllProduct(this.callback, this.state.gToken);
+    }
+
     render(){
-        return(
-            <Tabs defaultActiveKey="2"  >
-                <TabPane tab={<span><Icon type="dashboard" />Dashboard</span>} key="1" >
-                    <Dashboard/>
-                </TabPane>
-                <TabPane tab={<span><Icon type="form" />Product Entry</span>} key="2">
-                    <ProductEntry/>
-                </TabPane>
-                <TabPane tab={<span><Icon type="copy" />Create Version</span>} key="3">
-                    <CreateVersion/>
-                </TabPane>
-                <TabPane tab={<span><Icon type="setting" />Create Product</span>} key="4">
-                    <CreateProduct />
-                </TabPane>
-            </Tabs>
-        )
+        if(this.state.showLoader && !this.props.products){
+            return <Loader />
+        }else{
+            return(
+                <Tabs defaultActiveKey="1"  >
+                    <TabPane tab={<span><Icon type="dashboard" />Dashboard</span>} key="1" >
+                        <Dashboard products={this.props.products}/>
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="form" />Machine Entry</span>} key="2">
+                        <MachineEntry products={this.props.products}/>
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="copy" />Create Version</span>} key="3">
+                        <CreateVersion products={this.props.products}/>
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="setting" />Create Product</span>} key="4">
+                        <CreateProduct products={this.props.products}/>
+                    </TabPane>
+                </Tabs>
+            )
+        }
     }
 }
 
-export default ProductCatalogue
+function mapStateToProps(state) {
+    return{
+        products: state.Products.products
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return{
+        getProducts: (data) => {
+            dispatch({
+                type: actionType.PRODUCTS,
+                value: data
+            })
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductCatalogue)
