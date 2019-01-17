1b8c702f1dd22002dca312aeeb04af839381b93f
Anand Singh <sanand926@gmail.com>
2019-01-17 12:29:04 +0530

    Added: order status tab

--- src/admin/pages/orders/orders.jsx
+++ src/admin/pages/orders/orders.jsx
@@ -1,29 +1,74 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
 import {Tabs, Icon} from 'antd';
 import AllocateOrder from './allocate_order/allocate_order';
-import OrderList from './order_list/order_list'
+import OrderList from './order_list/order_list';
+import Loader from '../../../components/ui/loader/loader'
 import './orders.css'
+//
+import * as actionType from '../../../store/actions/action-type'
+import {GetToken} from '../../../helpers/token'
+import {getOrders} from '../../../services/apis/orders'
 
 const TabPane = Tabs.TabPane;
 
 class Orders extends Component {
+    state = {
+        gToken: GetToken(),
+        showLoader: true
+    }
+
+    callback = (data) => {
+        if(data.status === 200) {
+            this.props.dispatchOrders(data.data);
+            this.setState({showLoader: false});
+        }
+    }
+
+    componentDidMount () {
+        getOrders(this.callback, this.state.gToken);
+    }
 
     render() {
-        return(
-            <Tabs defaultActiveKey="2" >
-                <TabPane tab={<span><Icon type="dashboard" />Dashboard</span>} key="1" >
-                    Orders Dashboard
-                </TabPane>
-                <TabPane tab={<span><Icon type="ordered-list" />Order List</span>} key="2">
-                    <OrderList/>
-                </TabPane>
-                <TabPane tab={<span><Icon type="form" />Allocate Order</span>} key="3">
-                    <AllocateOrder/>
-                </TabPane>
-            </Tabs>
-        )
+        if(this.state.showLoader && !this.props.orders){
+            return <Loader/>
+        }else if(this.props.orders){
+            return(
+                <Tabs defaultActiveKey="4" >
+                    <TabPane tab={<span><Icon type="dashboard" />Dashboard</span>} key="1" >
+                        Orders Dashboard
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="ordered-list" />Order List</span>} key="2">
+                        <OrderList Orders={this.props.order}/>
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="edit" />Change Status</span>} key="3">
+                        Change Order Status
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="form" />Allocate Order</span>} key="4">
+                        <AllocateOrder Orders={this.props.order}/>
+                    </TabPane>
+                </Tabs>
+            )
+        }
+    }
+}
+
+function mapStateToProps (state) {
+    return {
+        orders: state.Orders.orders
+    }
+}
+
+function mapDispatchToProps (dispatch) {
+    return {
+        dispatchOrders: (data) => {
+            dispatch({
+                type: actionType.ORDERS,
+                value: data
+            })
+        }
     }
 }
 
 
-export default Orders
+export default connect(mapStateToProps, mapDispatchToProps)(Orders)
