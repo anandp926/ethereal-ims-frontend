9b4fe43a424fa43badcf6ad07d1d1f6fd4d80b6b
Anand Singh <sanand926@gmail.com>
2019-01-17 12:17:59 +0530

    Added: orders reducer

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -3,14 +3,16 @@ import login from './login';
 import basic from './basic';
 import company from './company';
 import users from './users';
-import products from './product_catalog'
+import products from './product_catalog';
+import orders from './orders'
 
 const rootReducer = combineReducers({
     Login: login,
     Basic: basic,
     Company: company,
     Users: users,
-    Products: products
+    Products: products,
+    Orders: orders
 });
 
 export default rootReducer
