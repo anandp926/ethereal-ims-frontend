dd0af934cd4d1d97301d9f8a03bf06773fb52e0f
Anand Singh <sanand926@gmail.com>
2019-01-24 11:53:57 +0530

    Modified: Added successful popup and sorted list

--- src/admin/pages/orders/allocate_order/order_detail/order_detail.jsx
+++ src/admin/pages/orders/allocate_order/order_detail/order_detail.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '../../../../../components/form/button/button';
 
 const DetailTemplate = () => {
@@ -29,75 +29,79 @@ const DetailTemplate = () => {
     )
 }
 
-class OrderDetail extends Component {
-    
-    render(){
-        let unproceedOrder, filterVendor;
-        if(this.props.Orders.length > 0) {
-            unproceedOrder = this.props.Orders.filter((orders) => orders.orderStatus.allocated === false);
-            if(unproceedOrder.length > 0) {
-                filterVendor = this.props.companies.filter((vendor) => vendor._id === unproceedOrder[0].vendorId)
-            }
+const OrderDetail= (props) => {
+    let unproceedOrder, filterVendor;
+    if(props.Orders.length > 0) {
+        unproceedOrder = props.Orders.filter((orders) => orders.orderStatus.allocated === false);
+        if(unproceedOrder.length > 0) {
+            filterVendor = props.companies.filter((vendor) => vendor._id === unproceedOrder[0].vendorId)
         }
-        
-        if(unproceedOrder !== undefined){
-            if(unproceedOrder.length > 0){
-                return(
-                    <table border="1">
-                        <thead>
-                            <tr>
-                            {
-                                filterVendor !== undefined && filterVendor.length > 0 && (
-                                    <td colSpan="4">
+    }
+    
+    if(unproceedOrder !== undefined){
+        if(unproceedOrder.length > 0){
+            return(
+                <table border="1">
+                    <thead>
+                        {
+                            filterVendor !== undefined && filterVendor.length > 0 && (
+                                <tr>
+                                    <td colSpan={unproceedOrder[0].products !== undefined && unproceedOrder[0].products.length > 0 ? '4' : '3'}>
                                         <h3><b>{filterVendor[0] ? filterVendor[0].name : 'Yet to be add'}</b></h3>
                                         {filterVendor[0] ? filterVendor[0].location : ''}
                                     </td>
-                                )
-                            }
-                            </tr>
-                            <tr>
-                                <th>Sr. </th>
-                                <th>Product Name</th>
-                                <th>Serial Number</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                unproceedOrder[0].products !== undefined && unproceedOrder[0].products.length > 0 && (
-                                    unproceedOrder[0].products.map((product, index) => (
-                                        <tr key={product._id}>
-                                            <td>{index}</td>
-                                            <td>{product.name}</td>
-                                            <td>{product.machineSRN}</td>
-                                            <td>
-                                                <span><a href="javascript:;" onClick={() => this.props.removeProduct(product, unproceedOrder[0]._id)}>Remove</a></span>
-                                            </td>
-                                        </tr>
-                                    ))
-                                )
-                            }
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td colSpan="4" className="text-right">
                                     {
                                         unproceedOrder[0].products !== undefined && unproceedOrder[0].products.length > 0
-                                        ? <Button isType="primary" onClick={() => this.props.proceedOrder(unproceedOrder[0]._id)}>Proceed</Button>
-                                        : null
+                                        ? null
+                                        : 
+                                        <td >
+                                            <span><a href="javascript:;" onClick={() => props.removeOrder(unproceedOrder[0]._id)}>Remove</a></span>
+                                        </td>
                                     }
-                                </td>                       
-                            </tr>
-                        </tfoot>
-                    </table>
-                )
-            }else{
-                return <DetailTemplate/>
-            }
-        }else {
+                                </tr>
+                            )
+                        }
+                        <tr>
+                            <th>Sr. </th>
+                            <th>Product Name</th>
+                            <th>Serial Number</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            unproceedOrder[0].products !== undefined && unproceedOrder[0].products.length > 0 && (
+                                unproceedOrder[0].products.map((product, index) => (
+                                    <tr key={product._id}>
+                                        <td>{index}</td>
+                                        <td>{product.name}</td>
+                                        <td>{product.machineSRN}</td>
+                                        <td>
+                                            <span><a href="javascript:;" onClick={() => props.removeProduct(product, unproceedOrder[0]._id)}>Remove</a></span>
+                                        </td>
+                                    </tr>
+                                ))
+                            )
+                        }
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan="4" className="text-right">
+                                {
+                                    unproceedOrder[0].products !== undefined && unproceedOrder[0].products.length > 0
+                                    ? <Button isType="primary" onClick={() => props.proceedOrder(unproceedOrder[0]._id)}>Proceed</Button>
+                                    : null
+                                }
+                            </td>                       
+                        </tr>
+                    </tfoot>
+                </table>
+            )
+        }else{
             return <DetailTemplate/>
         }
-        
+    }else {
+        return <DetailTemplate/>
     }
 }
 
