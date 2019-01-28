b103aa5b5fb89194922c002f42a80090eb9a3f29
Anand Singh <sanand926@gmail.com>
2019-01-28 18:03:44 +0530

    Modified: Added company product serialNumber in form

--- src/distributor/pages/issues/new/isuue-form.jsx
+++ src/distributor/pages/issues/new/isuue-form.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
 import {Modal} from 'antd'
 import Form from '../../../../components/form/form';
 import Loader from '../../../../components/ui/loader/loader'
@@ -9,8 +10,10 @@ import TextArea from '../../../../components/form/form-controls/text-area';
 import Button from '../../../../components/form/button/button';
 import Heading from '../../../../components/form/heading/heading';
 //
+import * as actionType from '../../../../store/actions/action-type'
 import {GetToken} from '../../../../helpers/token';
 import {createIssue} from '../../../../services/apis/issues';
+import {getUserCompany} from '../../../../services/apis/user';
 
 
 class IssueForm extends Component {
@@ -26,7 +29,8 @@ class IssueForm extends Component {
         subjectWarn: false,
         messageWarn: false,
         showLoader: false,
-        errMsg: ''
+        errMsg: '',
+        showUCLoader: true
     }
 
     success = () => {
@@ -43,11 +47,26 @@ class IssueForm extends Component {
         })
     }
 
+    getCompanyCallback= (data) => {
+        if(data.status === 200 || data.status === 304){
+            this.setState({showUCLoader: false});
+            this.props.dispatchUserCompany(data.data);
+        }else{
+            console.log(data.response)
+        }
+    }
+
+    componentDidMount(){
+        const {gToken} = this.state;
+        if(gToken){
+            getUserCompany(this.getCompanyCallback, gToken)
+        }
+    }
+
     callback = (data) => {
         if(data.status === 200){
             this.success();
             this.setState({showLoader: false, errMsg: ''});
-            console.log(data.data);
         }else {
             this.error();
             this.setState({showLoader: false, errMsg: ''});
@@ -108,61 +127,91 @@ class IssueForm extends Component {
     }    
 
     render(){
-        const productsName = ['Ethereal Ray', 'Ethereal Halo', 'Ethereal Pentagram'];
-        const productsNumber = ['er12asd ', 'er73dsf', 'er19iyu'];
+        const productsName = ['Pentagram', 'Ray', 'Halo'];
+        var productsNumber = [];
+        if(this.props.userComapny){
+            this.props.userComapny.products && (
+                this.props.userComapny.products.map((product) => productsNumber.push(product.machineSRN))
+            )
+        }
 
-        return(
-            <div className="container">
-                <div className="issue-form">
-                    <Heading heading={"Raise Issue"}/>
-                    <Form onSubmitHandler={this.onFormSubmit}>
-                        <AutoComplete  
-                            onSelectChange={(value) => this.setState({productName:value})}
-                            isRequired={true}
-                            name="productsName"
-                            labelName="Product Name"
-                            dataSource={productsName} 
-                            placeholder="Select a Product"
-                            classValue={this.state.productNameWarn ? 'inputField-outline' : null}
-                        />
-                        <AutoComplete  
-                            onSelectChange={(value) => this.setState({serialNumber:value})}
-                            isRequired={true}
-                            name="productsNumber"
-                            labelName="Product Number"
-                            dataSource={productsNumber} 
-                            placeholder="Select Product Number"
-                            classValue={this.state.serialNumberWarn ? 'inputField-outline' : null}
-                        />
-                        <InputType 
-                            type="text"
-                            value={this.state.subject}
-                            name="subject" 
-                            labelName="Subject"
-                            placeholder="Enter subject"
-                            onInputChange={this.onInputChange}
-                            isRequired={true}
-                            classValue={this.state.subjectWarn ? 'inputField-outline' : null}
-                        />
-                        <TextArea 
-                            name="message"
-                            labelName="Description" 
-                            placeholder="Enter Description" 
-                            value={this.state.message} 
-                            onInputChange={this.onInputChange} 
-                            isRequired={true}
-                            classValue={this.state.messageWarn ? 'inputField-outline' : null}
-                        />
-                        { this.state.errorMsg ? <ErrorBox errorMsgs={this.state.errorMsg} /> : null}
-                        { this.state.showLoader ? <Loader>Creating...</Loader> : null }
-                        <div className="issue-form-button" >
-                            <Button isType='primary' htmlTypes='submit' isBlock={true}>Submit</Button>
-                        </div>
-                    </Form>
+        if(this.state.showUCLoader) {
+            return (
+                <div className="container">
+                    <Loader/>
                 </div>
-            </div>
-        )
+            )
+        }else {
+            return(
+                <div className="container">
+                    <div className="issue-form">
+                        <Heading heading={"Raise Issue"}/>
+                        <Form onSubmitHandler={this.onFormSubmit}>
+                            <AutoComplete  
+                                onSelectChange={(value) => this.setState({productName:value})}
+                                isRequired={true}
+                                name="productsName"
+                                labelName="Product Name"
+                                dataSource={productsName} 
+                                placeholder="Select a Product"
+                                classValue={this.state.productNameWarn ? 'inputField-outline' : null}
+                            />
+                            <AutoComplete  
+                                onSelectChange={(value) => this.setState({serialNumber:value})}
+                                isRequired={true}
+                                name="productsNumber"
+                                labelName="Product Number"
+                                dataSource={productsNumber} 
+                                placeholder="Select Product Number"
+                                classValue={this.state.serialNumberWarn ? 'inputField-outline' : null}
+                            />
+                            <InputType 
+                                type="text"
+                                value={this.state.subject}
+                                name="subject" 
+                                labelName="Subject"
+                                placeholder="Enter subject"
+                                onInputChange={this.onInputChange}
+                                isRequired={true}
+                                classValue={this.state.subjectWarn ? 'inputField-outline' : null}
+                            />
+                            <TextArea 
+                                name="message"
+                                labelName="Description" 
+                                placeholder="Enter Description" 
+                                value={this.state.message} 
+                                onInputChange={this.onInputChange} 
+                                isRequired={true}
+                                classValue={this.state.messageWarn ? 'inputField-outline' : null}
+                            />
+                            { this.state.errorMsg ? <ErrorBox errorMsgs={this.state.errorMsg} /> : null}
+                            { this.state.showLoader ? <Loader>Creating...</Loader> : null }
+                            <div className="issue-form-button" >
+                                <Button isType='primary' htmlTypes='submit' isBlock={true}>Submit</Button>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
+            )
+        }<Loader/>
     }
 }
 
-export default IssueForm
+function mapStateToProps (state) {
+    return{
+        userComapny: state.Users.company
+    }
+}
+
+function mapDispatchToProps (dispatch) {
+    return{
+        dispatchUserCompany: (data) => {
+            dispatch({
+                type: actionType.USER_COMPANY,
+                value: data
+            })
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(IssueForm)
