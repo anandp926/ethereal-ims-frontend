7b683369d2327021f65ddfabaebdb639762e5d71
Anand Singh <sanand926@gmail.com>
2019-01-28 18:04:35 +0530

    Added: Added company prosduct request and stats

--- src/distributor/pages/dashboard/dashboard.jsx
+++ src/distributor/pages/dashboard/dashboard.jsx
@@ -1,25 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
 import Card from '../../../components/ui/card/card'
 import CardHeading from '../../../components/ui/card/card-heading/card-heading'
 import Divider from '../../../components/ui/divider/divider'
-import './dashboard.css'
+import AnalogClock from '../../../components/ui/analog_watch/analog_watch';
+import '../../../common_css/dashboard.css'
 import { Tree, Icon } from 'antd';
+//
+import {getUserCompany} from '../../../services/apis/user';
+import * as actionType from '../../../store/actions/action-type';
+import {GetToken} from '../../../helpers/token'
 
 const TreeNode = Tree.TreeNode;
 
 class Dashboard extends Component {
 
-    onSelect = (selectedKeys, info) => {
-        console.log('selected', selectedKeys, info);
-      }
+    state = {
+        gToken: GetToken(),
+        showLoader: true
+    }
+
+    callback = (data) => {
+        if(data.status === 200){
+            this.props.dispatchUserCompany(data.data);
+        }else{
+            console.log(data.response)
+        }
+    }
+
+    componentDidMount(){
+        const {gToken} = this.state;
+        if(gToken){
+            getUserCompany(this.callback, gToken)
+        }
+    }
 
     render(){
+        var haloCount=0, rayCount=0, pentagramCount=0;
+        if(this.props.userComapny){
+            if(this.props.userComapny.products !== undefined) {
+                this.props.userComapny.products.map((product) => {
+                    if(product.name === 'Halo'){
+                        haloCount++;
+                    }else if(product.name === 'Pentagram'){
+                        pentagramCount++;
+                    }else if(product.name === 'Ray'){
+                        rayCount++;
+                    }
+                });
+            }
+        }
+
         return(
             <div className="dashboard">
                 <div className="dashboard-card-list">
                     <div className="dashboard-card-item">
                         <Card>
-                            <CardHeading title="Inventory" classValue="dashboard-card-heading"/>
+                            <CardHeading title="Products" classValue="dashboard-card-heading"/>
                             <Divider/>
                             <div className="dashboard-inventory-detail">
                                 <Tree
@@ -27,15 +64,10 @@ class Dashboard extends Component {
                                     defaultExpandedKeys={['0-0']}
                                     onSelect={this.onSelect}
                                 >
-                                    <TreeNode title={<b>Available Machines</b>} key="0-0">
-                                        <TreeNode icon={<img className="ray-icon" />} title="Ray (3)" key="0-0-0-0" />
-                                        <TreeNode icon={<img className="halo-icon" />} title="Halo (6)" key="0-0-0-1" />
-                                        <TreeNode icon={<img className="pentagram-icon" />} title="Pentagram (1)" key="0-0-0-2" />
-                                    </TreeNode>
-                                    <TreeNode title={<b>Pending Orders</b>} key="0-1">
-                                        <TreeNode icon={<img className="ray-icon" />} title="Ray (0)" key="0-0-1-0" />
-                                        <TreeNode icon={<img className="halo-icon" />} title="Halo (12)" key="0-0-2-0" />
-                                        <TreeNode icon={<img className="pentagram-icon" />} title="Pentagram (0)" key="0-0-2-1" />
+                                    <TreeNode title={<b>Machines</b>} key="0-0">
+                                        <TreeNode icon={<img className="ray-icon" />} title={`Ray (${rayCount})`} key="0-0-0-0" />
+                                        <TreeNode icon={<img className="halo-icon" />} title={`Halo (${haloCount})`} key="0-0-0-1" />
+                                        <TreeNode icon={<img className="pentagram-icon" />} title={`Pentagram (${pentagramCount})`} key="0-0-0-2" />
                                     </TreeNode>
                                 </Tree>
                             </div>
@@ -43,50 +75,7 @@ class Dashboard extends Component {
                     </div>
                     <div className="dashboard-card-item">
                         <Card>
-                            <CardHeading title="Sales" classValue="dashboard-card-heading" />
-                            <Divider/>
-                            <div className="dashboard-inventory-detail">
-                                <Tree
-                                    showIcon
-                                    defaultExpandedKeys={['0-0']}
-                                    onSelect={this.onSelect}
-                                >
-                                    <TreeNode title={<b>Toatl Purchased</b>} key="0-0">
-                                        <TreeNode icon={<img className="ray-icon" />} title="Ray (3)" key="0-0-0-0" />
-                                        <TreeNode icon={<img className="halo-icon" />} title="Halo (6)" key="0-0-0-1" />
-                                        <TreeNode icon={<img className="pentagram-icon" />} title="Pentagram (1)" key="0-0-0-2" />
-                                    </TreeNode>
-                                    <TreeNode title={<b>Total Sold</b>} key="0-1">
-                                        <TreeNode icon={<img className="ray-icon" />} title="Ray (0)" key="0-0-1-0" />
-                                        <TreeNode icon={<img className="halo-icon" />} title="Halo (12)" key="0-0-2-0" />
-                                        <TreeNode icon={<img className="pentagram-icon" />} title="Pentagram (0)" key="0-0-2-1" />
-                                    </TreeNode>
-                                </Tree>
-                            </div>
-                        </Card>
-                    </div>
-                    <div className="dashboard-card-item">
-                        <Card>
-                            <CardHeading title="Issues" classValue="dashboard-card-heading"/>
-                            <Divider/>
-                            <div className="dashboard-inventory-detail">
-                                <Tree
-                                    showIcon
-                                    defaultExpandedKeys={['0-0']}
-                                    onSelect={this.onSelect}
-                                >
-                                    <TreeNode title={<b>Total Issues</b>} key="0-0">
-                                        <TreeNode icon={<img className="ray-icon" />} title="Ray (3)" key="0-0-0-0" />
-                                        <TreeNode icon={<img className="halo-icon" />} title="Halo (6)" key="0-0-0-1" />
-                                        <TreeNode icon={<img className="pentagram-icon" />} title="Pentagram (1)" key="0-0-0-2" />
-                                    </TreeNode>
-                                    <TreeNode title={<b>Pending Issues</b>} key="0-1">
-                                        <TreeNode icon={<img className="ray-icon" />} title="Ray (0)" key="0-0-1-0" />
-                                        <TreeNode icon={<img className="halo-icon" />} title="Halo (12)" key="0-0-2-0" />
-                                        <TreeNode icon={<img className="pentagram-icon" />} title="Pentagram (0)" key="0-0-2-1" />
-                                    </TreeNode>
-                                </Tree>
-                            </div>
+                            <AnalogClock />
                         </Card>
                     </div>
                 </div>
@@ -95,4 +84,21 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard
+function mapStateToProps (state) {
+    return {
+        userComapny: state.Users.company
+    }
+}
+
+function mapDispatchToProps (dispatch) {
+    return {
+        dispatchUserCompany: (data) => {
+            dispatch({
+                type: actionType.USER_COMPANY,
+                value: data
+            })
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
